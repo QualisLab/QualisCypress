22d02a5ccca5e13dc5c90abe4c0ca84146e0bc82
NicoQualis <105435471+NicoQualis@users.noreply.github.com>
2022-09-27 12:35:21 -0300

    first

---
+++ README.md
@@ -0,0 +1,36 @@
+# qualis-testing
+
+
+> Cypress 10+ with Cucumber boilerplate project.
+
+### 💻 Topics
+
+Integrated with:
+
+- [x] https://github.com/badeball/cypress-cucumber-preprocessor
+- [x] https://github.com/bahmutov/cypress-esbuild-preprocessor
+- [x] https://www.npmjs.com/package/multiple-cucumber-html-reporter
+- [x] https://github.com/cucumber/json-formatter
+
+(+ bundlers: https://github.com/badeball/cypress-cucumber-preprocessor/tree/master/examples)
+
+- ## 💻 Pre-requisites
+
+Before you use this project you only need to have Node Js installed in your computer.
+
+https://nodejs.org/es/download/
+
+Also, download and install the JSON fomatter depending on your OS
+
+https://github.com/cucumber/json-formatter
+
+## 🚀 Install the project
+
+Install project dependencies with: npm i
+
+## 🚀 Run the demo: 
+Open the terminal and run: npm run cypress:execution
+
+## 🚀 Run the test: 
+Open the terminal and run: .\runTest.ps1
+(Oracle Instant Client must be installed in C:\   >>>   https://www.oracle.com/database/technologies/instant-client/winx64-64-downloads.html)
---
+++ cypress.config.js
@@ -0,0 +1,63 @@
+const { defineConfig } = require("cypress");
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const addCucumberPreprocessorPlugin =
+  require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
+const createEsbuildPlugin =
+  require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
+const oracledb = require("oracledb");
+
+/**
+ * @type {Cypress.PluginConfig}
+ */
+
+ oracledb.initOracleClient({libDir: 'C:\\instantclient_21_6'});
+
+ const queryData = async(query, dbconfig) => {
+   let conn;
+   try{
+       conn = await oracledb.getConnection(dbconfig);
+       result = await conn.execute(query);
+       console.log(result)
+       return result
+   }catch(err){
+       console.log("Error===>"+err)
+       return err
+   } finally{
+     if(conn){
+       try{
+         conn.close();
+       }catch(err){
+         console("Error===>"+err);
+       }
+     }
+   }
+ }
+
+module.exports = defineConfig({
+  env: {
+    db: {
+        "user": "user",
+        "password": "pass",
+        "connectString" : "adress"
+    }
+  },
+  e2e: {
+    async setupNodeEvents(on, config) {
+      const bundler = createBundler({
+        plugins: [createEsbuildPlugin(config)],
+      });
+      on("task", {
+        sqlQuery: (query) => {
+          return queryData(query, config.env.db);
+        },
+      });
+      on("file:preprocessor", bundler);
+      await addCucumberPreprocessorPlugin(on, config);
+
+      return config;
+    },
+    specPattern: "cypress/e2e/features/*.feature",
+    baseUrl: "https://www.saucedemo.com",
+    chromeWebSecurity: false,
+  },
+});
